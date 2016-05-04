bf809757921f13280a963bf9b0e108ea3ad7a952
BurgerLUA <mattwasnothere@gmail.com>
2016-05-03 19:06:46 -0700

    Update

--- lua/autorun/client/cl_csp_core.lua
+++ lua/autorun/client/cl_csp_core.lua
@@ -10,26 +10,28 @@ function CSP_DrawSpawns()
 	
 	local ply = LocalPlayer()
 	local MapData = CSP_GetMapData()
-
-	for k,v in pairs(MapData) do
 	
-		local ScreenTable = (v.pos - Vector(0,0,20)):ToScreen()
+	if MapData then
+		for k,v in pairs(MapData) do
 		
-		local PosX = ScreenTable.x
-		local PosY = ScreenTable.y
-		local IsVisible = ScreenTable.visible and ply:IsLineOfSightClear( v.pos )
-		
-		if IsVisible then
-		
-			local Base = 500
-		
-			local DistanceMul = math.Clamp( (Base - EyePos():Distance(v.pos)),0,Base )
-		
-			surface.SetDrawColor( Color(255,255,255,255) )
-			surface.SetMaterial( SpawnMaterial	) -- If you use Material, cache it!
-			surface.DrawTexturedRectRotated( PosX, PosY, DistanceMul, DistanceMul ,CurTime()*10 )
-		end
+			local ScreenTable = (v.pos - Vector(0,0,20)):ToScreen()
+			
+			local PosX = ScreenTable.x
+			local PosY = ScreenTable.y
+			local IsVisible = ScreenTable.visible and ply:IsLineOfSightClear( v.pos )
+			
+			if IsVisible then
+			
+				local Base = 500
+			
+				local DistanceMul = math.Clamp( (Base - EyePos():Distance(v.pos)),0,Base )
+			
+				surface.SetDrawColor( Color(255,255,255,255) )
+				surface.SetMaterial( SpawnMaterial	) -- If you use Material, cache it!
+				surface.DrawTexturedRectRotated( PosX, PosY, DistanceMul, DistanceMul ,CurTime()*10 )
+			end
 
+		end
 	end
 
 end
@@ -40,27 +42,31 @@ function CSP_DrawLight()
 
 	local MapData = CSP_GetMapData()
 	
-	for k,v in pairs(MapData) do
+	if MapData then
 	
-		local FlashLight = DynamicLight( k + 1000 )
+		for k,v in pairs(MapData) do
 		
-		local Base = 500
-		local DistanceMul = math.Clamp( (Base - EyePos():Distance(v.pos)),0,Base )
+			local FlashLight = DynamicLight( k + 1000 )
+			
+			local Base = 500
+			local DistanceMul = math.Clamp( (Base - EyePos():Distance(v.pos)),0,Base )
 
-		if FlashLight then
-			local FadeOutTime = FrameTime()*2
-			FlashLight.Pos = v.pos - Vector(0,0,20)
-			FlashLight.Size = DistanceMul
-			FlashLight.DieTime = CurTime() + FadeOutTime*10
-			FlashLight.Decay = 1000 / FadeOutTime
-			FlashLight.MinLight = 0
-			FlashLight.r = 255
-			FlashLight.g = 255
-			FlashLight.b = 255
+			if FlashLight then
+				local FadeOutTime = FrameTime()*2
+				FlashLight.Pos = v.pos - Vector(0,0,20)
+				FlashLight.Size = DistanceMul
+				FlashLight.DieTime = CurTime() + FadeOutTime*10
+				FlashLight.Decay = 1000 / FadeOutTime
+				FlashLight.MinLight = 0
+				FlashLight.r = 255
+				FlashLight.g = 255
+				FlashLight.b = 255
+			end
+			
 		end
-		
-	end
 
+	end
+	
 end
 
 hook.Add("Think","CSP_DrawLight",CSP_DrawLight)
@@ -80,26 +86,6 @@ function CSP_CreateSpawns()
 				end
 				table.remove(CSP_CachedModels,k)
 			end
-			
-			--[[
-
-			for k,v in pairs(MapData) do
-			
-				print("SPAWNING")
-
-				local Ent = ents.CreateClientProp()
-				Ent:SetPos( v.pos - Vector(0,0,60) ) 
-				Ent:SetAngles( v.ang )
-				Ent:SetModel("models/hunter/tubes/tube2x2x025.mdl")
-				--Ent:SetMaterial("debug/debugwhite")
-				Ent:SetColor( Color(255,255,255,255) )
-				Ent:Spawn()
-				
-				table.Add(CSP_CachedModels,{Ent})
-
-			end
-			
-			--]]
 			
 			RenderedModels = true
 			
